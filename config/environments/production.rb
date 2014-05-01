d28441c1b9916f4b9cf43c99a97d2133e84a17c6
russellandrews <rwandrews62@gmail.com>
2014-05-01 16:11:21 -0400

    commiting before starting heroku process again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,6 +23,7 @@ Rails.application.configure do
   config.serve_static_assets = false
 
   # Compress JavaScripts and CSS.
+  config.assets.css_compressor = :yui
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
